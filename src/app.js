e108a1ef886e7b53ac0e999af682a8d6475f36fd
Ayush Shukla <ayushshukla50@gmail.com>
2025-03-05 20:27:16 +0530

    create multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,21 +3,32 @@ const express = require('express');
 const app = express();
 
 // Request handler
-app.get("/user", (request, response) => {
-    response.send({"firstName" : "Ayush", "lastName" : "Shukla"});
-})
-
-app.post("/user", (request, response) => {
-    response.send("Data saved sucessfully...");
-})
-
-app.delete("/user", (request, response) => {
-    response.send("Data deleted sucessfully...");
-})
-
-app.use("/test", (request, response) => {
-    response.send('Test...');
-})
+app.use("/user", 
+    (req, resp, next) => {
+        console.log("Handling route user 1!!");
+        // resp.send("User 1....");
+        next();
+    },
+    (req, resp, next) => {
+        console.log("Handling route user 2!!");
+        // resp.send("User 2....");
+        next();
+    },
+    (req, resp, next) => {
+        console.log("Handling route user 3!!");
+        // resp.send("User 3....");
+        next();
+    },
+    (req, resp, next) => {
+        console.log("Handling route user 4!!");
+        // resp.send("User 4....");
+        next();
+    },
+    (req, resp) => {
+        console.log("Handling route user 5!!");
+        resp.send("User 5....");
+    }
+)
 
 app.listen(7777, () => {
     console.log('Server is running on port 3000');
